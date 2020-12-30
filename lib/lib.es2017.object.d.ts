00988ff3e3f1b4a8996d1fb3b716f4b9bef570bf
uhyo <uhyo@uhy.ooo>
2020-12-30 09:49:05 +0900

    es2017.object

---
+++ lib/lib.es2017.object.d.ts
@@ -0,0 +1,44 @@
+/// <reference no-default-lib="true"/>
+
+interface ObjectConstructor {
+  /**
+   * Returns an array of values of the enumerable properties of an object
+   * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
+   */
+  values<T>(
+    o: T
+  ): string | number extends keyof T
+    ? T[string | number][]
+    : string extends keyof T
+    ? T[string][]
+    : number extends keyof T
+    ? T[number][]
+    : unknown[];
+
+  /**
+   * Returns an array of key/values of the enumerable properties of an object
+   * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
+   */
+  entries<T>(
+    o: T
+  ): [
+    string,
+    string | number extends keyof T
+      ? T[string | number]
+      : string extends keyof T
+      ? T[string]
+      : number extends keyof T
+      ? T[number]
+      : unknown
+  ][];
+
+  /**
+   * Returns an object containing all own property descriptors of an object
+   * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
+   */
+  getOwnPropertyDescriptors<T>(
+    o: T
+  ): { [P in keyof T]: TypedPropertyDescriptor<T[P]> } & {
+    [x: string]: PropertyDescriptor;
+  };
+}
---
+++ tests/es2017.object.ts
@@ -0,0 +1,31 @@
+/// <reference path="../dist/lib/lib.es2017.d.ts" />
+
+import { expectType } from "tsd";
+
+{
+  const obj1 = { foo: 123 };
+  const values1 = Object.values(obj1);
+  const entries1 = Object.entries(obj1);
+  expectType<unknown[]>(values1);
+  expectType<[string, unknown][]>(entries1);
+
+  const obj2: Record<string, number> = {};
+  const values2 = Object.values(obj2);
+  const entries2 = Object.entries(obj2);
+  expectType<number[]>(values2);
+  expectType<[string, number][]>(entries2);
+
+  const obj3 = ["foo", "bar", "baz"];
+  const values3 = Object.values(obj3);
+  const entries3 = Object.entries(obj3);
+  expectType<string[]>(values3);
+  expectType<[string, string][]>(entries3);
+}
+function test<T>(obj: T) {
+  const values = Object.values(obj);
+  expectType<unknown>(values[0]);
+
+  const entries = Object.entries(obj);
+  expectType<string>(entries[0][0]);
+  const v: unknown = entries[0][1];
+}
