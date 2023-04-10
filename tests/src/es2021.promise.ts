cd1a27e9fc8decd5713f03a1dcd93a8454d6a4d4
Aadit M Shah <aaditmshah@aadit.codes>
2023-04-10 14:34:32 +0530

    :white_check_mark: Add tests for promises

---
+++ tests/src/es2021.promise.ts
@@ -0,0 +1,8 @@
+import { expectType } from "tsd";
+
+const test = async (
+  values: Iterable<string | PromiseLike<PromiseLike<number>>>
+) => {
+  const result = await Promise.any(values);
+  expectType<string | number>(result);
+};
