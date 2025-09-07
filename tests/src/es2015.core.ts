1e15e936c0931259339e311ae08bcf4625e647aa
uhyo <uhyo@uhy.ooo>
2025-09-07 18:23:45 +0900

    fix: preserve call signatures of target object in Object.assign

--- tests/src/es2015.core.ts
+++ tests/src/es2015.core.ts
@@ -34,4 +34,19 @@ import { expectError, expectType } from "tsd";
   expectError(Object.getOwnPropertySymbols(null));
   const obj5 = Object.setPrototypeOf({ foo: 123 }, { bar: "wow" });
   expectType<{ foo: number } & { bar: string }>(obj5);
+
+  // https://github.com/uhyo/better-typescript-lib/issues/71
+  // Object.assign should preserve function callability
+  const func1 = () => 42;
+  const funcWithProps = Object.assign(func1, { baz: "hello" });
+  expectType<(() => number) & { baz: string }>(funcWithProps);
+  expectType<number>(funcWithProps()); // Should be callable
+  expectType<string>(funcWithProps.baz); // Should have the added property
+
+  const func2 = function(x: number): string { return x.toString(); };
+  const funcWithProps2 = Object.assign(func2, { value: 123 }, { flag: true });
+  expectType<((x: number) => string) & { value: number } & { flag: boolean }>(funcWithProps2);
+  expectType<string>(funcWithProps2(42)); // Should be callable
+  expectType<number>(funcWithProps2.value); // Should have added properties
+  expectType<boolean>(funcWithProps2.flag);
 }
